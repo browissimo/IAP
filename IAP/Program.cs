51c397fb31023338105ea9d88291bcbc8d7dee8c
Browissimo <browissimo@gmail.com>
2023-08-22 16:54:21 +0300

    Добавьте файлы проекта.

---
+++ IAP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using InstagramApiSharp;
+
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+using System.Threading.Channels;
+using IAP;
+
+namespace InstagramHashtagImageDownloader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var userSessionData = new UserSessionData();
+
+            await Console.Out.WriteLineAsync("Введи логин:");
+            userSessionData.UserName = Console.ReadLine();
+
+            await Console.Out.WriteLineAsync("Введи пароль");
+            userSessionData.Password = Console.ReadLine();
+
+            var directory = "C:\\Users\\fedunov\\Desktop\\c";
+
+            var menu = """
+                Выбери опцию:
+                1 - поиск фото по тегу
+                """;
+            await Console.Out.WriteLineAsync(menu);
+
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    var tagGrabber = new TagGrabber(userSessionData, directory);
+                    await tagGrabber.GrabByTag();
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            await Console.Out.WriteLineAsync("Впиши tag");
+            var tag = Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
---
+++ IAP/TagGrabber.cs
@@ -0,0 +1,100 @@
+﻿using InstagramApiSharp;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+
+namespace IAP
+{
+    public class TagGrabber
+    {
+        private readonly UserSessionData _user;
+        private readonly string _outputDirectory;
+
+        public TagGrabber(UserSessionData user, string outputDirectory)
+        {
+            _user = user;
+            _outputDirectory = outputDirectory;
+        }
+
+        public async Task GrabByTag()
+        {
+            Console.WriteLine("Введи tag");
+            var hashtag = Console.ReadLine();
+            var downloadPath = _outputDirectory; 
+
+            var api = InstaApiBuilder.CreateBuilder()
+                .SetUser(_user)
+                .Build();
+
+            if (!api.IsUserAuthenticated)
+            {
+                var loginResult = await api.LoginAsync();
+                if (!loginResult.Succeeded)
+                {
+                    Console.WriteLine($"Failed to login: {loginResult.Info.Message}");
+                    return;
+                }
+            }
+
+            await Console.Out.WriteLineAsync("User logged");
+
+            var lastId = string.Empty;
+            var tagFeed = await api.HashtagProcessor.GetTopHashtagMediaListAsync(hashtag, PaginationParameters.MaxPagesToLoad(500));
+
+            for (int i = 0; i < 500; i++)
+            {
+
+                if (!string.IsNullOrEmpty(lastId))
+                {
+                    tagFeed = await api.HashtagProcessor.GetTopHashtagMediaListAsync(hashtag, PaginationParameters.MaxPagesToLoad(500).StartFromMaxId(lastId));
+                }
+
+
+
+                if (!tagFeed.Succeeded)
+                {
+                    Console.WriteLine($"Failed to retrieve hashtag media: {tagFeed.Info.Message}");
+                    return;
+                }
+
+                await Console.Out.WriteLineAsync("Feed gated");
+
+                foreach (var mediaItem in tagFeed.Value.Medias)
+                {
+                    var owner = mediaItem.User.UserName;
+
+                    foreach (var image in mediaItem.Images)
+                    {
+                        var imageUrl = image.Uri;
+                        var imageBytes = await api.HttpClient.GetByteArrayAsync(imageUrl);
+
+                        var fileNameWithoutExtension = $"{owner}";
+                        var fileExtension = ".jpg";
+                        var fileName = $"{fileNameWithoutExtension}{fileExtension}";
+                        var filePath = Path.Combine(downloadPath, fileName);
+
+                        int suffix = 1;
+                        while (File.Exists(filePath))
+                        {
+                            fileNameWithoutExtension = $"{owner}_{suffix}";
+                            fileName = $"{fileNameWithoutExtension}{fileExtension}";
+                            filePath = Path.Combine(downloadPath, fileName);
+                            suffix++;
+                        }
+
+                        File.WriteAllBytes(filePath, imageBytes);
+
+                        Console.WriteLine($"Image saved: {filePath}");
+                    }
+                }
+
+                lastId = tagFeed.Value.Medias.LastOrDefault()?.InstaIdentifier;
+            }
+
+
+
+            Console.WriteLine("Image downloading completed.");
+        }
+
+    }
+
+}

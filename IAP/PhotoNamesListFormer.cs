3654cba25e846d4e857779dd40de3b3eae464e17
Browissimo <browissimo@gmail.com>
2023-08-31 16:52:56 +0300

    fix

---
+++ IAP/PhotoNamesListFormer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAP
+{
+    public class PhotoNamesListFormer
+    {
+        private string _directory;
+
+        public PhotoNamesListFormer(string directory)
+        {
+            _directory = directory;
+        }
+
+        public void NamesFormer()
+        {
+            string folderPath = _directory;
+            string[] files = Directory.GetFiles(folderPath);
+
+            List<string> uniqueNames = new List<string>();
+
+            foreach (string filePath in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                int lastUnderscoreIndex = fileName.LastIndexOf('_'); // Находим индекс последнего символа подчеркивания
+                if (lastUnderscoreIndex >= 0)
+                {
+                    string nameWithoutSuffix = fileName.Substring(0, lastUnderscoreIndex); // Удаляем часть после последнего символа подчеркивания
+                    uniqueNames.Add(nameWithoutSuffix);
+                }
+            }
+
+            uniqueNames = uniqueNames.Distinct().ToList(); // Получаем уникальные имена
+
+            File.WriteAllLines("profile_names.txt", uniqueNames); // Сохраняем имена в файл
+
+            Console.WriteLine("Уникальные имена сохранены в файл profile_names.txt.");
+        }
+    }
+}
--- IAP/Program.cs
+++ IAP/Program.cs
@@ -12,19 +12,13 @@ namespace InstagramHashtagImageDownloader
     {
         static async Task Main(string[] args)
         {
-            var userSessionData = new UserSessionData();
-
-            await Console.Out.WriteLineAsync("Введи логин:");
-            userSessionData.UserName = Console.ReadLine();
-
-            await Console.Out.WriteLineAsync("Введи пароль");
-            userSessionData.Password = Console.ReadLine();
 
             var directory = "C:\\Users\\fedunov\\Desktop\\c";
 
             var menu = """
                 Выбери опцию:
                 1 - поиск фото по тегу
+                2 - собрать имена файлов в список
                 """;
             await Console.Out.WriteLineAsync(menu);
 
@@ -33,19 +27,26 @@ namespace InstagramHashtagImageDownloader
             switch (option)
             {
                 case "1":
+
+                    var userSessionData = new UserSessionData();
+
+                    await Console.Out.WriteLineAsync("Введи логин:");
+                    userSessionData.UserName = Console.ReadLine();
+
+                    await Console.Out.WriteLineAsync("Введи пароль");
+                    userSessionData.Password = Console.ReadLine();
+
                     var tagGrabber = new TagGrabber(userSessionData, directory);
                     await tagGrabber.GrabByTag();
                     break;
+                case "2":
+                    var photoNamesList = new PhotoNamesListFormer(directory);
+                    photoNamesList.NamesFormer();
+                    break;
+
                 default:
                     break;
-            }
-
-
-
-            await Console.Out.WriteLineAsync("Впиши tag");
-            var tag = Console.ReadLine();
-
-            
+            }         
 
 
         }
